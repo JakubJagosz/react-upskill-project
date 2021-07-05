a9259c79993e19172361bb342078f4db84ba6323
Lukasz Korzeniowski <lkorzeniowski2@objectivity.co.uk>
2021-07-05 17:13:28 +0200

    Add search functionality.

--- src/ArticleSearch/ArticleSearchBar.jsx
+++ src/ArticleSearch/ArticleSearchBar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Grid, InputAdornment, TextField } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 
-function ArticleSearchBar() {
+function ArticleSearchBar({searchValue, handleChange}) {
     return (
         <Grid item xs={12} md={12}>
             <TextField
                 label="Search Article"
                 size={"medium"}
+                value={searchValue}
+                onChange={handleChange}
                 InputProps={{
                     endAdornment: (
                         <InputAdornment position="start">
--- src/ArticleSearch/ArticleSearchContainer.jsx
+++ src/ArticleSearch/ArticleSearchContainer.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import ArticleSearchBar from './ArticleSearchBar';
 import Article from './Article';
 import { articles } from './resources/Articles';
 import { Grid } from '@material-ui/core';
 
-const ArticleSearchContainer = () => (
-    <Grid container spacing={2} item xs={6} md={6}>
-        <ArticleSearchBar />
-        {articles.map((article) => (
-            <Article title={article.title} snippet={article.snippet} />
-        ))}
-    </Grid>
-);
+
+const ArticleSearchContainer = () => {
+    const [searchValue, setSearchValue] = useState('')
+
+    const [filteredArticles, setFilteredArticles] = useState([])
+    
+    const handleChange = (e) => {
+        const {value} = e.target
+        debugger
+        setSearchValue(value.toLowerCase())
+    }
+
+    const handleSearch = useCallback(() => {
+        const filteredArticles = articles.filter(article => article.title.toLowerCase().includes(searchValue))
+        setFilteredArticles(filteredArticles)
+    },[searchValue])
+
+    useEffect(() => {
+        handleSearch()
+    }, [searchValue, handleSearch])
+
+    return (
+        <Grid container spacing={2} item xs={6} md={6}>
+            <ArticleSearchBar searchValue={searchValue} handleChange={handleChange} />
+            {filteredArticles.map((article) => (
+                <Article key={article.pageid} title={article.title} snippet={article.snippet} url={article.url} />
+            ))}
+        </Grid>
+    )
+};
 
 ArticleSearchContainer.propTypes = {};
 
